2278b5de061781470cd13ee7388ee8189ba43ed5
Oscar Quintero <oscar24quintero@hotmail.com>
2021-11-21 23:25:12 -0600

    Script de Windows flujo para clase online

--- script-optimizacion.bat
+++ script-optimizacion.bat
@@ -1,3 +1,20 @@
 @ECHO OFF
-ECHO "Hola"
 
+TITLE Oscar Quintero - Seminario SOs 2021B
+ECHO Iniciando Flujo de Trabajo Escolar
+
+ECHO Revisiones de rutina de la Computadora
+POWERCFG /batteryreport output C:\estado-bateria.html
+START C:\estado-bateria.html
+START /B taskmgr.exe
+START /B dfrgui.exe
+
+ECHO Abriendo Ventanas necesarias para la clase 
+
+START chrome.exe "https://moodle2.cucei.udg.mx"
+START chrome.exe "https://classroom.google.com"
+START chrome.exe "https://calendar.google.com"
+START chrome.exe "https://apps.google.com/meet/"
+ECHO Creando Archivo para apuntes....
+TYPE nul > ApuntesDeClase.md
+START ApuntesDeClase.md
